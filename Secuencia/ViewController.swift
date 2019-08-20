3a866bb281ad67bda9e8c9a369b6d0ad30d43394
Alumno <ulsa@iMacD109.local>
2019-08-20 14:36:20 -0700

    segundo intento

--- Secuencia/ViewController.swift
+++ Secuencia/ViewController.swift
@@ -10,8 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func doTapConejo(_ sender: Any) {
+        if imgSecuencia.isAnimating {
+            imgSecuencia.stopAnimating()
+        } else {
+            imgSecuencia.startAnimating()
+        }
+    }
+    @IBOutlet weak var imgSecuencia: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Arreglo de UIImage con las imagenes de la secuencia
+        var imagenesSecueniaConejo : [UIImage] = []
+        imagenesSecueniaConejo.append(UIImage(named: "conejo1")!) //Nombre del Asset
+        imagenesSecueniaConejo.append(UIImage(named: "conejo2")!)
+        imagenesSecueniaConejo.append(UIImage(named: "conejo3")!)
+        imagenesSecueniaConejo.append(UIImage(named: "conejo4")!)
+        imagenesSecueniaConejo.append(UIImage(named: "conejo5")!)
+        imagenesSecueniaConejo.append(UIImage(named: "conejo6")!)
+        imagenesSecueniaConejo.append(UIImage(named: "conejo7")!)
+        
+        imgSecuencia.animationImages = imagenesSecueniaConejo
+        imgSecuencia.animationDuration = 0.7
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
